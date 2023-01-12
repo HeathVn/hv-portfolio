318f528dae6b2bac4a4c54fbce1fa1ddf1ca6d6b
Heathvonn Styles <hvonn1039@gmail.com>
2023-01-12 13:28:20 -0500

    Jan 2023 Website Cleanup

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -12,7 +12,7 @@ const Banner = () => {
         <div className='bannerInnerCon'>
            {/* <a href="https://www.flaticon.com/free-icons/inclusion" title="inclusion icons">Inclusion icons created by Freepik - Flaticon</a> */}
            
-           <h3 className='inspiration'>Fueled by creativity and eager to positively impact the world</h3>
+           <h3 className='inspiration'>Fueled by creativity and eager to make a positive impact on the world</h3>
         </div>
         </div>
     </div>
